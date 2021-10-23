cb243084d3155d818c931fe17187b7cfe0012833
Ivan Boban <ivan.boban911@gmail.com>
2021-10-23 21:58:42 +0200

    initialize client with cra

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      My cook book
+      <a href="http://asd.com">dasdadasd</a>
+    </div>
+  );
+}
+
+export default App;
