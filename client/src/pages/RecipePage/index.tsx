f82843f4677ab595707e4f2ad69da1e3ba38fb83
Ivan Boban <ivan.boban@decode.agency>
2021-10-24 20:57:03 +0200

    Added logic for Recipe page

--- client/src/pages/RecipePage/index.tsx
+++ client/src/pages/RecipePage/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import useLoading from "../../hooks/useLoading";
+import useRecipePage from "./useRecipePage";
 
 export default function RecipePage() {
+  const [isLoading, { disableLoading }] = useLoading(true);
+  const { recipe } = useRecipePage(disableLoading);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return <div>Recipe page</div>;
 }
