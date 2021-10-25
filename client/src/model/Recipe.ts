c0ff953b72f041423d9c5a183edbee02d9692a63
Ivan Boban <ivan.boban@decode.agency>
2021-10-25 21:21:49 +0200

    Updated naming

--- client/src/model/Recipe.ts
+++ client/src/model/Recipe.ts
@@ -1,11 +1,8 @@
+import Ingredient from "./Ingredient";
+
 export default interface Recipe {
   id: number;
   name: string;
   ingredients: [Ingredient];
   ratings: [number];
-}
-
-interface Ingredient {
-  name: string;
-  quantity: number;
 }
