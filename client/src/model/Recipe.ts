0a23c9a47b32379e5038e2890b3181f2ec5b5e5f
Ivan Boban <ivan.boban@decode.agency>
2021-10-25 21:57:18 +0200

    Updated Recipe interface to match DTO's

--- client/src/model/Recipe.ts
+++ client/src/model/Recipe.ts
@@ -3,6 +3,8 @@ import Ingredient from "./Ingredient";
 export default interface Recipe {
   id: number;
   name: string;
+  cookingTime: number;
+  numberOfServings: number;
   ingredients: [Ingredient];
   ratings: [number];
 }
