60e919ebcf13b1092f31ad6187837ae58452658d
Ivan Boban <ivan.boban@decode.agency>
2021-11-01 13:57:13 +0100

    Added ingredient and preparation step hooks

---
+++ client/src/hooks/useIngredients.ts
@@ -0,0 +1,33 @@
+import React from "react";
+import Ingredient from "../model/Ingredient";
+
+type Handlers = {
+  addIngredient: () => void;
+  removeIngredient: (index: number) => void;
+};
+
+const ingredient = { name: "", quantity: 0 };
+
+export default function useIngredients(
+  initialIngredients?: Array<Ingredient>
+): [Array<Ingredient>, Handlers] {
+  const [ingredients, setIngredients] = React.useState(
+    initialIngredients ? initialIngredients : [ingredient]
+  );
+
+  const handlers = React.useMemo(
+    () => ({
+      addIngredient: () => {
+        setIngredients([...ingredients, ingredient]);
+      },
+      removeIngredient: (index: number) => {
+        setIngredients(
+          ingredients.filter((_, itemIndex) => itemIndex !== index)
+        );
+      },
+    }),
+    [ingredients]
+  );
+
+  return [ingredients, handlers];
+}
---
+++ client/src/hooks/usePreparationSteps.ts
@@ -0,0 +1,29 @@
+import React from "react";
+
+type Handlers = {
+  addPreparationStep: () => void;
+  removePreparationStep: (index: number) => void;
+};
+export default function usePreparationSteps(
+  initialPreparationSteps?: Array<string>
+): [Array<string>, Handlers] {
+  const [preparationSteps, setPreparationSteps] = React.useState(
+    initialPreparationSteps ? initialPreparationSteps : [""]
+  );
+
+  const handlers = React.useMemo(
+    () => ({
+      addPreparationStep: () => {
+        setPreparationSteps([...preparationSteps, ""]);
+      },
+      removePreparationStep: (index: number) => {
+        setPreparationSteps(
+          preparationSteps.filter((_, itemIndex) => itemIndex !== index)
+        );
+      },
+    }),
+    [preparationSteps]
+  );
+
+  return [preparationSteps, handlers];
+}
